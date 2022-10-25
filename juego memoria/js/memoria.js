7b66018d65ea3500d2f11960481e58620d6f0fe1
Javier <javiermardom@gmail.com>
2022-10-25 19:00:32 +0200

    meter emoticonos en array aleatoriamente

--- juego memoria/js/memoria.js
+++ juego memoria/js/memoria.js
@@ -3,7 +3,7 @@ let maxColumnas = prompt('¿Cuántas columnas quieres?');
 let arrayTablero = [];
 
 // Nos aseguramos de que el número total de casillas nos permita
-// que no haya ningñuna pareja suelta
+// que no haya ninguna pareja suelta
 while ((maxFilas * maxColumnas) % 2 != 0) {
 
     window.alert("La multiplicación de las filas y las columnas tiene que ser par");
@@ -13,20 +13,28 @@ while ((maxFilas * maxColumnas) % 2 != 0) {
 
 }
 
-
 // Creamos el array
+
+//Array de emoticonos
+
+let imagenes = ['&#9961;&#65039', '&#128566;&#8205;&#127787;&#65039', '&#128126',
+'&#128125', '&#128121', '&#128760', '&#127888', '&#129398', '&#129484', '&#128131'];
+
 
 for (let fila = 0; fila < maxFilas; fila++) {
     arrayTablero[fila] = new Array(maxColumnas);
 
+    //Por cada pasada de este for, elige un objeto aleatorio del array de emoticonos
+    //y lo mete en el array
     for (let columna = 0; columna < maxColumnas; columna++) {
-        arrayTablero[fila][columna] = '';
+
+        let random = imagenes[Math.floor(Math.random() * imagenes.length)]
+        arrayTablero[fila][columna] = random;
 
     }
 }
 
 // Creamos y dibujamos el tablero con el array
-
 document.write('<table>');
 
 for (let i = 0; i < maxFilas; i++) {
@@ -42,19 +50,3 @@ for (let i = 0; i < maxFilas; i++) {
 document.write('</table>');
 
 console.log(arrayTablero);
-
-//Posicionamos las imagenes
-let posFila;
-let posColumna;
-let numImg = maxFilas * maxColumnas;
-let contadorImg;
-
-while (contadorImg < numImg) {
-    posFila = Math.floor(Math.random() * maxFilas);
-    posColumna = Math.floor(Math.random() * maxColumnas);
-
-    if (arrayTablero[posFila][posColumna] != 'MINA') {
-        arrayTablero[posFila][posColumna] = 'MINA';
-        contadorMinas++;
-    };
-}
