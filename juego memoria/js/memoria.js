d3054e8ef00c9f7793f4d20f9bd1d8e55f3b7ae5
Javier <javiermardom@gmail.com>
2022-10-28 17:09:54 +0200

    fallos

--- juego memoria/js/memoria.js
+++ juego memoria/js/memoria.js
@@ -1,28 +1,32 @@
+//Pregunta la cantidad de filas y columnas que quiere el usuario
 let maxFilas = prompt('¿Cuántas filas quieres?');
 let maxColumnas = prompt('¿Cuántas columnas quieres?');
 let arrayTablero = [];
 
 // Nos aseguramos de que el número total de casillas nos permita
 // que no haya ninguna pareja suelta
-while ((maxFilas * maxColumnas) % 2 != 0) {
+while ((maxFilas * maxColumnas) % 2 != 0 || maxFilas * maxColumnas > 100) {
 
-    window.alert("La multiplicación de las filas y las columnas tiene que ser par");
+    if ((maxFilas * maxColumnas) % 2 != 0) {
+
+        window.alert("La multiplicación de las filas y las columnas tiene que ser par");
+
+    }
+
+    if (maxFilas * maxColumnas > 100) {
+
+        window.alert("Sólo puede haber un máximo de 100 casillas");
+
+    }
 
     maxFilas = prompt('¿Cuántas filas quieres?');
     maxColumnas = prompt('¿Cuántas columnas quieres?');
 
 }
 
-//Array de emoticonos
-let imagenes = ['&#9961;&#65039', '&#128566;&#8205;&#127787;&#65039', '&#128126',
-    '&#128125', '&#128121', '&#128760', '&#127888', '&#129398', '&#128406', '&#128131'];
-
-let posFila;
-let posColumna;
-let contadorImagenes;
-
 // Creamos el array
 for (let fila = 0; fila < maxFilas; fila++) {
+
     arrayTablero[fila] = new Array(maxColumnas);
 
     for (let columna = 0; columna < maxColumnas; columna++) {
@@ -33,23 +37,44 @@ for (let fila = 0; fila < maxFilas; fila++) {
 
 }
 
+//Array de emoticonos
+let imagenes = ['&#9961;&#65039', '&#128566;&#8205;&#127787;&#65039', '&#128126',
+    '&#128125', '&#128121', '&#128760', '&#127888', '&#129398', '&#128406', '&#128131'];
+
 //Por cada pasada de este for, elige en orden un objeto del array de emoticonos
 //y lo mete en el array aleatoriamente
+
+let contador = 0;
+let suma = 0;
+
 for (let i = 0; i < maxFilas; i++) {
 
     for (let j = 0; j < maxColumnas; j++) {
 
         for (let x = 0; x < imagenes.length; x++) {
 
-            let posFila = Math.floor(Math.random() * maxFilas);
-            let posColumna = Math.floor(Math.random() * maxColumnas);
+            while (contador < 2) {
 
-            if (arrayTablero[posFila][posColumna] != imagenes[x]) {
-                arrayTablero[posFila][posColumna] = imagenes[x];
+                let posFila = Math.floor(Math.random() * maxFilas);
+                let posColumna = Math.floor(Math.random() * maxColumnas);
+
+                if (arrayTablero[posFila][posColumna] == '') {
+                    arrayTablero[posFila][posColumna] = imagenes[x];
+                    contador++;
+                    suma++;
+                }
+
             }
-        }
-    }
 
+            if(suma > (maxFilas * maxColumnas)){
+                break;
+            }
+
+            contador = 0;
+
+        }
+
+    }
 
 }
 
